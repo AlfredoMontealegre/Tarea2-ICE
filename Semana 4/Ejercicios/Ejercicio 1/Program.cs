fcf9a82bda36dc220200022650cab58171d814b5
Blanca <bazeledon@uamv.edu.ni>
2025-09-19 09:01:37 -0600

    Archivos Blanca
    Se realizaron nuevas actualizaciones

---
+++ Semana 4/Ejercicios/Ejercicio 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+
+        //Calcular el factorial de un númemro entero El prograa se repetirá mientras el usuario lo decida//
+
+        static int Factorial(int número)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= número; i++);
+            {
+                int i = 0;
+                factorial = factorial * i;
+            }
+            return factorial;
+            Console.WriteLine($"El factorial del {número} es {factorial}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número entero para calcular su factorial: ");
+            int número = int.Parse(Console.ReadLine());
+            Factorial(número);
+            Console.WriteLine("¿Desea calcular otro factorial? (s/n)");
+            string respuesta = Console.ReadLine();
+            if (respuesta.ToLower() == "s")
+            {
+                Main(args);
+            }
+            else
+            {
+                Console.WriteLine("Gracias por usar el programa. ¡Hasta luego!");
+            }
+
+        }
+    }
+}
---
+++ Semana 4/Evaluación/Ejercicio 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+
+        //Desarrollla una función en C# que reciba como parametro una cadena de texto ingresada por el usuario y devuelva la cantidad de vocales que contiene//
+
+        static int contadorDeVocales(string letras)
+        {
+            int vocales = 0;
+            letras = letras.ToUpper();
+            foreach (char v in letras)
+            {
+                if (v == 'A' || v == 'E' || v == 'I' || v == 'O' || v == 'U')
+                {
+                    vocales++;
+                }
+            }
+            Console.WriteLine($"La cantidad de vocales del texto anterior son: {vocales}");
+            return vocales;
+            
+
+        }
+        static void Main()
+        {
+            Console.WriteLine("Ingrese una palabra o frase: ");
+            string texto = Console.ReadLine();
+
+            int cantidadDeVocales = contadorDeVocales(texto);
+        }
+    }
+}
